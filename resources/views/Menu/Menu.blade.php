04609387956080e3aa6dcda435851744fd8b5f74
Isaías Gamaliel Martínez Juárez <135473698+IsaiasGMJ@users.noreply.github.com>
2024-01-10 19:17:38 -0600

    Add files via upload

---
+++ resources/views/Menu/Menu.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menu</title>
+</head>
+<body>
+    <h1>Aqui van a ir los curso</h1>
+    <a href="/Dashboard">Dashboard</a>
+    <A href="/Pregunta">Pregunta</A>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::view('/Menu',('/Menu/Menu'));
+Route::view('/Pregunta',('/Pregunta/Pregunta'));
+Route::view('/Dashboard',('/Dashboard/Dashboard'));
